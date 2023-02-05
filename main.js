3a2c4b5d294a0eb71e4478dfedc16e1ebf66f346
abdullo2000 <goldabdullo@gmail.com>
2023-02-05 23:10:58 +0500

    second

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 function logInfo(name, age, gender) {
-    return console.log(`Name: ${name},\nAge: ${age},\nGender: ${gender}`);
+    return `Name: ${name},\nAge: ${age},\nGender: ${gender}`;
 }
-
-// console.log(logInfo("Xabijon", 19, "erkak")); 
 
 document.body.innerHTML = "<pre>" + logInfo("Xabijon", 19, "erkak") + "</pre>";
